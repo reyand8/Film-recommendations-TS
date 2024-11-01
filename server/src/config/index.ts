bd7ab2bbda39edc4cd44189a72725d5e0122d128
Andrey <andreeey.gg@gmail.com>
2024-11-01 15:25:57 +0100

    add config

---
+++ server/src/config/index.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL: string = 'https://api.themoviedb.org/3/';
+
+export const API_KEY: string = '<YOUR_KEY>';
+
+export const IMAGE_BASE_PATH: string = 'https://image.tmdb.org/t/p/w300';
